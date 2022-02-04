9385d679927e001de322f0c4defbd7cd6841aa16
Osas <Osaghae2305@gmail.com>
2022-02-04 13:36:11 +0000

    Simple queue finished

--- SimpleQueue.py
+++ SimpleQueue.py
@@ -1,49 +1,48 @@
-
-def enqueue(aqueue,item,cRear,qsize):   # function to add items 
-    cRear = cRear + 1 % qsize
-    aqueue[cRear] = item
-    qsize += 1
+class Queue():
+    def __init__(self):
+        self.queue = ['', '', '', '', '']
+        self.maxS = len(self.queue)
+        self.size = 0
+        self.front = 0
+        self.rear = -1
     
-    return aqueue ,cRear ,qsize
+    def enqueue(self, item):
+        self.rear = (self.rear + 1) % self.maxS
+        self.queue[self.rear] = item
+        self.size += 1
+        print(self.queue)
 
-def dequeue(cFront,qsize):
-    cFront = cFront + 1 % qsize
-    qsize -= 1
+    def dequeue(self):
+        self.front = (self.front + 1) % self.maxS
+        self.size -= 1
+        print(self.queue[self.front],"is in front of the queue")
+
+    def isFull(self):
+        if self.size == self.maxS:
+            print("Queue is full")
+            return True   
+
+    def Isempty(self):
+        if self.size == 0:
+            print("Queue is empty")
+            return True    
     
-    return cFront, qsize
-
-
-
-def Isfull(qsize,msize):   # can be called by the enqueue function 
-    if qsize == msize:
-        print("Queue is full!!")
-        return True
-
-
-def Isempty(qsize):    # can be called by the dequeue function
-    if qsize == 0:
-        print("Queue is empty")
-        return True
-
-
-mylist = [" "," "," "," "," "]   #initialise an empty queue
-maxsize=5
-size=0
-front = 0
-rear = -1
-run = True
 ########## MAin program ###########
 
+run = True
+myQueue = Queue()
+
 while run:
-    if Isfull(size, len(mylist)):
+    if myQueue.isFull():
         run = False
     else:
         choice = int(input("1.Add to queue\n2.Remove from queue\n"))
         if choice == 1:
-            value = input("Enter value to queue\n")
-            mylist, rear, size = enqueue(mylist,value, rear, size)
-            print(mylist)
+            name = input("Add name to queue\n")
+            myQueue.enqueue(name)
         else:
-            print("Removing from queue")
-            front, size = enqueue(front, size)
+            if myQueue.Isempty():
+                pass
+            else:
+                myQueue.dequeue()
             
